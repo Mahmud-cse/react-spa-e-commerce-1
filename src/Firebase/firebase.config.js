3c27d7e01b6772585bb4c83f794cdaf2f30cc5f3
Mahmud-cse <mahmudhasan555666@gmail.com>
2021-10-14 21:48:43 +0600

    firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBvCcz2AsCGlEe4jz2Xxn6ZVUpkh25RhXo",
-  authDomain: "ema-john-sp.firebaseapp.com",
-  projectId: "ema-john-sp",
-  storageBucket: "ema-john-sp.appspot.com",
-  messagingSenderId: "819305492808",
-  appId: "1:819305492808:web:f9c3cbdaa78d51974f037d"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
